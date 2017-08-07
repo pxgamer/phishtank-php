206be25677a6e842a8a43ac652acc153b00bfd09
PXgamer <owzie123@gmail.com>
2017-08-07 14:10:28 +0100

    Updated unit tests to cover new style

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -1,14 +1,46 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
 use pxgamer\PhishTank\Hook;
 
-class MainTest extends PHPUnit_Framework_TestCase
+/**
+ * Class MainTest
+ */
+class MainTest extends TestCase
 {
+    /**
+     * @var Hook
+     */
+    private $result;
 
-    public function testCheckUrl()
+    /**
+     * MainTest constructor.
+     * @param null $name
+     * @param array $data
+     * @param string $dataName
+     */
+    public function __construct($name = null, array $data = [], $dataName = '')
     {
-        $result = Hook::checkUrl('https://github.com');
+        parent::__construct($name, $data, $dataName);
 
-        $this->assertArrayHasKey('meta', $result);
+        $this->result = new Hook('https://github.com');
+    }
+
+    /**
+     * Check that the meta attribute is a valid instance of the Meta class
+     */
+    public function testUrlMetaValid()
+    {
+        $this->assertObjectHasAttribute('meta', $this->result);
+        $this->assertTrue(is_a($this->result->meta(), 'pxgamer\PhishTank\Meta'));
+    }
+
+    /**
+     * Check that the results attribute is a valid instance of the Results class
+     */
+    public function testUrlResultsValid()
+    {
+        $this->assertObjectHasAttribute('results', $this->result);
+        $this->assertTrue(is_a($this->result->results(), 'pxgamer\PhishTank\Results'));
     }
 }
